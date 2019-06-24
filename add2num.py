350facbeca4e7c93fa28f1f9d3701c79d46d5a84
abhilash2501 <52146638+abhilash2501@users.noreply.github.com>
2019-06-24 16:08:01 +0530

    Add files via upload

---
+++ add2num.py
@@ -0,0 +1,15 @@
+def inpu():
+    a=int(input("enter a:"))
+    b=int (input("enter b:"))
+    return a,b;
+
+def add(a,b):
+    return a+b;
+def output(a,b):
+    print(add(a,b))
+    return;
+def main():
+    c,d=inpu()
+    output(c,d)
+    return;
+    
